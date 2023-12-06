e032fb6ce201c938d645d45d46a91a3a5c0770d4
j1642 <60148902+j1642@users.noreply.github.com>
2023-12-06 11:51:53 -0500

    ENH: Add day 2 part 1

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,7 +1,63 @@
-pub fn day_2_1(text: &str) -> i32 {
-    let mut total = 0;
+pub fn day_2_1(text: &str) -> u32 {
+    // Return sum of possible game IDs if the bag contains 12 red,
+    // 13 green, and 14 blue cubes
+    let bag_contents = [12, 13, 14];
+    let mut game_id_sum = 0;
+
     for line in text.lines() {
-        total += 1;
+        let mut line = line.split(':');
+
+        let game_id = line.next().unwrap().split(' ');
+        let game_id: u32 = game_id.last().unwrap().parse::<u32>().unwrap();
+
+        let mut is_game_valid = true;
+        let game = line.next().unwrap().split(';');
+
+        for round in game {
+            if !is_game_valid {
+                break;
+            }
+            for hand in round.split(',') {
+                let mut rgb_max = [0, 0, 0];
+
+                let mut tokens = hand.split(' ');
+                tokens.next();
+                let count: u32 = tokens.next().unwrap().parse::<u32>().unwrap();
+                let color = tokens.next().unwrap();
+                match color {
+                    "red" => {
+                        if count > rgb_max[0] {
+                            rgb_max[0] = count;
+                        }
+                    }
+                    "green" => {
+                        if count > rgb_max[1] {
+                            rgb_max[1] = count;
+                        }
+                    }
+                    "blue" => {
+                        if count > rgb_max[2] {
+                            rgb_max[2] = count;
+                        }
+                    }
+                    _ => panic!("invalid color: {color}"),
+                }
+                // If one round of a game is invalid, the game is invalid
+                for i in 0..bag_contents.len() {
+                    if rgb_max[i] > bag_contents[i] {
+                        is_game_valid = false;
+                        break;
+                    }
+                    is_game_valid = true;
+                }
+                if !is_game_valid {
+                    break;
+                }
+            }
+        }
+        if is_game_valid {
+            game_id_sum += game_id;
+        }
     }
-    return total;
+    return game_id_sum;
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,4 @@
-use day2::{day_2_1};
+use day2::day_2_1;
 use std::fs;
 
 fn main() {
@@ -9,6 +9,11 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
-    let day2_1_ans = day_2_1(_short_text);
-    println!("day 2.1 ={day2_1_ans}");
+    let day2_1_short = day_2_1(_short_text);
+    println!("day 2.1 (short) expected=8, got={day2_1_short}");
+    let day2_1 = day_2_1(&_text);
+    println!("day 2.1 = {day2_1}");
+
+    //let day2_2 = day2_2(_short_text);
+    //println!("day 2.2 = {day2_2}");
 }
