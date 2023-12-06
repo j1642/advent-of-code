b47891bbfcd8f232fa04ca38eb7269b23c2dda9f
j1642 <60148902+j1642@users.noreply.github.com>
2023-12-06 13:51:54 -0500

    ENH: Add day 2 part 2

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -61,3 +61,47 @@ pub fn day_2_1(text: &str) -> u32 {
     }
     return game_id_sum;
 }
+
+pub fn day_2_2(text: &str) -> u32 {
+    // Return the sum of the products of the minimum amount of
+    // each cube color that would make each game possible
+    let mut total = 0;
+
+    for line in text.lines() {
+        let mut line = line.split(':');
+        // Discard "Game id:"
+        line.next();
+
+        let game = line.next().unwrap().split(';');
+        let mut rgb_max = [0; 3];
+
+        for round in game {
+            for hand in round.split(',') {
+                let mut tokens = hand.split(' ');
+                tokens.next();
+                let count: u32 = tokens.next().unwrap().parse::<u32>().unwrap();
+                let color = tokens.next().unwrap();
+                match color {
+                    "red" => {
+                        if count > rgb_max[0] {
+                            rgb_max[0] = count;
+                        }
+                    }
+                    "green" => {
+                        if count > rgb_max[1] {
+                            rgb_max[1] = count;
+                        }
+                    }
+                    "blue" => {
+                        if count > rgb_max[2] {
+                            rgb_max[2] = count;
+                        }
+                    }
+                    _ => panic!("invalid color: {color}"),
+                }
+            }
+        }
+        total += rgb_max.iter().fold(1, |acc, &x| acc * x);
+    }
+    return total;
+}
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,19 +1,21 @@
-use day2::day_2_1;
+use day2::{day_2_1, day_2_2};
 use std::fs;
 
 fn main() {
-    let _text = fs::read_to_string("day2.txt").expect("Should have read the file");
-    let _short_text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    let text = fs::read_to_string("day2.txt").expect("Should have read the file");
+    let short_text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
-    let day2_1_short = day_2_1(_short_text);
+    let day2_1_short = day_2_1(short_text);
     println!("day 2.1 (short) expected=8, got={day2_1_short}");
-    let day2_1 = day_2_1(&_text);
-    println!("day 2.1 = {day2_1}");
+    let day2_1_ans = day_2_1(&text);
+    println!("day 2.1 = {day2_1_ans}");
 
-    //let day2_2 = day2_2(_short_text);
-    //println!("day 2.2 = {day2_2}");
+    let day2_2_short = day_2_2(short_text);
+    println!("day 2.2 (short) expected=2286, got={day2_2_short}");
+    let day2_2_ans = day_2_2(&text);
+    println!("day 2.2 = {day2_2_ans}");
 }
