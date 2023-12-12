86a05335a6787012ac99ead305304eac04437541
j1642 <60148902+j1642@users.noreply.github.com>
2023-12-12 00:17:11 -0500

    ENH: Add 8.2

--- day8/src/lib.rs
+++ day8/src/lib.rs
@@ -1,12 +1,110 @@
 use std::collections::HashMap;
+use std::str::Lines;
 
-pub fn day_8_1(text: &str) -> u32 {
+pub fn day_8_1(text: &str) -> u64 {
+    // Return number of steps required to reach node ZZZ from node AAA
+    // The number should be a multiple of length of the first line of text
+    let mut lines = text.lines();
+    let directions = lines.next().unwrap();
+    lines.next();
+    let graph = build_graph(&mut lines);
+
+    let mut current = "AAA";
+    let mut count_steps = 0;
+    loop {
+        for direction in directions.chars() {
+            if direction == 'R' {
+                current = graph[current][1];
+            } else if direction == 'L' {
+                current = graph[current][0];
+            } else {
+                panic!("invalid direction: {direction}");
+            }
+            count_steps += 1;
+        }
+        if current == "ZZZ" {
+            break;
+        }
+    }
+
+    return count_steps;
+}
+
+pub fn day_8_2(text: &str) -> u64 {
     // Return number of steps required to reach node ZZZ from node AAA
     // The number should be a multiple of length of the first line of text
     let mut lines = text.lines();
     let directions = lines.next().unwrap();
     lines.next();
 
+    let mut current_nodes: Vec<&str> = vec![];
+    let mut graph = HashMap::new();
+    loop {
+        let line = lines.next();
+        if line == None {
+            break;
+        }
+
+        let (node, edges) = line.unwrap().split_once(" = (").unwrap();
+
+        let (edge1, mut edge2) = edges.split_once(", ").unwrap();
+        edge2 = edge2.trim_end_matches(')');
+
+        graph.insert(node, [edge1, edge2]);
+        if node.chars().last().unwrap() == 'A' {
+            current_nodes.push(node);
+        }
+    }
+
+    let mut distances_to_end: Vec<u64> = vec![0; current_nodes.len()];
+
+    for i in 0..current_nodes.len() {
+        let mut count_steps = 0;
+        let mut current = current_nodes[i];
+        'middle: loop {
+            for direction in directions.chars() {
+                if direction == 'R' {
+                    current = graph[current][1];
+                } else if direction == 'L' {
+                    current = graph[current][0];
+                } else {
+                    panic!("invalid direction '{direction}'");
+                }
+                count_steps += 1;
+                if current.chars().last().unwrap() == 'Z' {
+                    distances_to_end[i] = count_steps;
+                    break 'middle;
+                }
+            }
+        }
+    }
+
+    return least_common_multiple(distances_to_end);
+}
+
+fn least_common_multiple(mut nums: Vec<u64>) -> u64 {
+    let mut numerator;
+    let mut denom;
+
+    while nums.len() > 1 {
+        denom = greatest_common_divisor(nums[0], nums[1]);
+        numerator = nums.pop().unwrap() * nums.pop().unwrap();
+        let least_common_multiple = numerator / denom;
+        nums.push(least_common_multiple);
+    }
+
+    return nums[0];
+}
+
+fn greatest_common_divisor(mut a: u64, mut b: u64) -> u64 {
+    // Euclidean algorithm
+    while b != 0 {
+        (a, b) = (b, a % b);
+    }
+    return a;
+}
+
+fn build_graph<'a>(lines: &mut Lines<'a>) -> HashMap<&'a str, [&'a str; 2]> {
     let mut graph = HashMap::new();
     loop {
         let line = lines.next();
@@ -21,21 +119,5 @@ pub fn day_8_1(text: &str) -> u32 {
         graph.insert(node, [edge1, edge2]);
     }
 
-    let mut current = "AAA";
-    let mut count_steps = 0;
-    loop {
-        for direction in directions.chars() {
-            if direction == 'R' {
-                current = graph[current][1];
-            } else if direction == 'L' {
-                current = graph[current][0];
-            }
-            count_steps += 1;
-        }
-        if current == "ZZZ" {
-            break;
-        }
-    }
-
-    return count_steps;
+    return graph;
 }
--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,5 @@
-use day8::day_8_1;
+use day8::{day_8_1, day_8_2};
+//use std::env;
 use std::fs;
 
 fn main() {
@@ -15,12 +16,23 @@ ZZZ = (ZZZ, ZZZ)";
     //assert_eq!(ans_8_1, );
     println!("day 8_1 = {}", ans_8_1);
 
-    /*
     // Part 2
-    assert_eq!(day_8_2(short_text), 5905);
+    let short_text_2 = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+    assert_eq!(day_8_2(short_text_2), 6);
+
+    //env::set_var("RUST_BACKTRACE", "1");
 
     let ans_8_2 = day_8_2(&text);
-    //assert_eq!(ans_8_2, );
+    assert_eq!(ans_8_2, 12_324_145_107_121);
     println!("day 8_2 = {}", ans_8_2);
-    */
 }
