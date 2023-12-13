2f499bf41520075935f7dedac88548063c7259bd
j1642 <60148902+j1642@users.noreply.github.com>
2023-12-12 21:22:04 -0500

    ENH: Add 10.2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -8,8 +8,8 @@ pub fn day_10_1(text: &str) -> u32 {
         for (j, c) in line.chars().enumerate() {
             if c == 'S' {
                 // Adjust start indices because of added '.' rows and columns
-                start = (i+1, j+1);
-                row.push('.');
+                start = (i + 1, j + 1);
+                row.push('X');
             } else {
                 row.push(c);
             }
@@ -23,20 +23,216 @@ pub fn day_10_1(text: &str) -> u32 {
     return find_midpoint_distance(&mut matrix, start);
 }
 
+pub fn day_10_2(text: &str) -> u32 {
+    // Find squares bounded by the path
+    let mut matrix: Vec<Vec<char>> = vec![];
+    let mut start = (0, 0);
+
+    for (i, line) in text.lines().enumerate() {
+        let mut row: Vec<char> = vec![];
+        row.push('.');
+        for (j, c) in line.chars().enumerate() {
+            if c == 'S' {
+                // Adjust start indices because of added '.' rows and columns
+                start = (i + 1, j + 1);
+                row.push('|');
+            } else {
+                row.push(c);
+            }
+        }
+        row.push('.');
+        matrix.push(row);
+    }
+    matrix.insert(0, vec!['.'; matrix[0].len()]);
+    matrix.push(vec!['.'; matrix[0].len()]);
+
+    let orig_matrix = matrix.clone();
+
+    // 1. follow the path, mark with X
+    _ = find_midpoint_distance(&mut matrix, start);
+    // 2. flood fill the outside border, including diagonally
+    // matrix[0][0] is in an artificial row and column, not real data
+    flood_fill_exterior(&mut matrix, 0, 0);
+    // TODO: 3. get the original matrix with the junk chars outside the loop removed
+    let mut cleaned_matrix = matrix.clone();
+    for row in 0..matrix.len() {
+        for col in 0..matrix[0].len() {
+            if matrix[row][col] == 'X' {
+                cleaned_matrix[row][col] = orig_matrix[row][col];
+            }
+        }
+    }
+
+    let mut count_bounded_squares = 0;
+    for row in 0..matrix.len() {
+        for col in 0..matrix[0].len() {
+            if matrix[row][col] != 'X' && matrix[row][col] != 'O' {
+                // Even-odd rule for 2D graphics
+                if bounded_by_trail(&mut matrix, &cleaned_matrix, row, col) {
+                    count_bounded_squares += 1;
+                } else {
+                    matrix[row][col] = 'O';
+                }
+            }
+        }
+    }
+
+    return count_bounded_squares;
+}
+
+fn bounded_by_trail(
+    matrix: &mut Vec<Vec<char>>,
+    cleaned_matrix: &Vec<Vec<char>>,
+    row: usize,
+    col: usize,
+) -> bool {
+    // Return whether a point is bounded by the path using the even-odd rule
+    let directions: [&str; 2] = ["east", "west"];
+    let mut wall_counts: [bool; 2] = [false; 2];
+    let mut empty_wc_index = 0;
+
+    for dir in directions {
+        if dir == "east" && col == matrix[0].len() - 1 {
+            continue;
+        } else if dir == "west" && col == 0 {
+            continue;
+        }
+
+        let mut indices = vec![];
+        if dir == "west" {
+            indices = (0..col).rev().collect::<Vec<usize>>();
+        } else if dir == "east" {
+            indices = ((col + 1)..matrix[0].len()).collect::<Vec<usize>>();
+        }
+
+        let mut count_walls = 0;
+        for i in indices {
+            if matrix[row][i] == 'X' {
+                // Only |, F...J, and L...7 are walls for the even-odd rule
+                if cleaned_matrix[row][i] == '|' {
+                    count_walls += 1;
+                    continue;
+                }
+                let wall_pairs = [['F', 'J'], ['L', '7']];
+                for wall_pair in wall_pairs {
+                    if cleaned_matrix[row][i] == wall_pair[0] {
+                        if cleaned_matrix[row][i + 1] == wall_pair[1] {
+                            count_walls += 1;
+                            continue;
+                        }
+                        let mut count_dashes = 1;
+                        while cleaned_matrix[row][i + count_dashes] == '-' {
+                            count_dashes += 1;
+                        }
+                        if cleaned_matrix[row][i + count_dashes] == wall_pair[1] {
+                            count_walls += 1;
+                        }
+                    }
+                }
+            } else if matrix[row][i] == 'O' {
+                break;
+            }
+        }
+        if count_walls % 2 == 0 {
+            wall_counts[empty_wc_index] = false;
+            empty_wc_index += 1;
+        } else {
+            wall_counts[empty_wc_index] = true;
+            empty_wc_index += 1;
+        }
+    }
+    return wall_counts.contains(&true);
+}
+
+fn flood_fill_exterior(matrix: &mut Vec<Vec<char>>, row: usize, col: usize) {
+    matrix[row][col] = 'O';
+    // Check all 8 directions for chars besides O and X
+    let directions: Vec<&str>;
+    if row == 0 {
+        if col == 0 {
+            directions = vec!["e", "se", "s"];
+        } else if col == matrix[0].len() - 1 {
+            directions = vec!["s", "sw", "w"];
+        } else {
+            directions = vec!["e", "se", "s", "sw", "w"];
+        }
+    } else if row == matrix.len() - 1 {
+        if col == 0 {
+            directions = vec!["n", "ne", "e"];
+        } else if col == matrix[0].len() - 1 {
+            directions = vec!["n", "w", "nw"];
+        } else {
+            directions = vec!["n", "ne", "e", "w", "nw"];
+        }
+    } else if col == 0 {
+        directions = vec!["n", "ne", "e", "se", "s"];
+    } else if col == matrix[0].len() - 1 {
+        directions = vec!["n", "s", "sw", "w", "nw"];
+    } else {
+        directions = vec!["n", "ne", "e", "se", "s", "sw", "w", "nw"];
+    }
+
+    let mut chr: char;
+    let mut next_row: usize;
+    let mut next_col: usize;
+    for direction in directions {
+        match direction {
+            "ne" => {
+                next_row = row - 1;
+                next_col = col + 1;
+            }
+            "e" => {
+                next_row = row;
+                next_col = col + 1;
+            }
+            "se" => {
+                next_row = row + 1;
+                next_col = col + 1;
+            }
+            "sw" => {
+                next_row = row + 1;
+                next_col = col - 1;
+            }
+            "w" => {
+                next_row = row;
+                next_col = col - 1;
+            }
+            "nw" => {
+                next_row = row - 1;
+                next_col = col - 1;
+            }
+            "n" => {
+                next_row = row - 1;
+                next_col = col;
+            }
+            "s" => {
+                next_row = row + 1;
+                next_col = col;
+            }
+            &_ => panic!("invalid direction"),
+        }
+        chr = matrix[next_row][next_col];
+        if chr != 'O' && chr != 'X' && chr != '*' {
+            flood_fill_exterior(matrix, next_row, next_col);
+        }
+    }
+}
+
 fn find_midpoint_distance(matrix: &mut Vec<Vec<char>>, start: (usize, usize)) -> u32 {
     // Erase path to location loc1 so loc2 must follow a different path
     let moves = moves_from_start(&matrix, start);
-    matrix[start.0][start.1] = '.';
+    matrix[start.0][start.1] = 'X';
 
     let mut loc1 = moves[0];
     let mut loc2 = moves[1];
-    
+
     let mut count_steps = 1;
     while loc1 != loc2 {
         loc1 = step(matrix, loc1);
         loc2 = step(matrix, loc2);
         count_steps += 1;
     }
+    matrix[loc1.0][loc1.1] = 'X';
 
     return count_steps;
 }
@@ -44,54 +240,44 @@ fn find_midpoint_distance(matrix: &mut Vec<Vec<char>>, start: (usize, usize)) ->
 fn step(matrix: &mut Vec<Vec<char>>, curr: (usize, usize)) -> (usize, usize) {
     let pipes = "L|7J-F";
     let next;
+    let choice1;
+    let choice2;
 
     match matrix[curr.0][curr.1] {
         'L' => {
-            if pipes.contains(matrix[curr.0][curr.1 + 1]) {
-                next = (curr.0, curr.1 + 1);
-            } else {
-                next = (curr.0 - 1, curr.1);
-            }
+            choice1 = (curr.0, curr.1 + 1);
+            choice2 = (curr.0 - 1, curr.1);
         }
         '|' => {
-            if pipes.contains(matrix[curr.0 - 1][curr.1]) {
-                next = (curr.0 - 1, curr.1);
-            } else {
-                next = (curr.0 + 1, curr.1);
-            }
+            choice1 = (curr.0 - 1, curr.1);
+            choice2 = (curr.0 + 1, curr.1);
         }
         '7' => {
-            if pipes.contains(matrix[curr.0][curr.1 - 1]) {
-                next = (curr.0, curr.1 - 1);
-            } else {
-                next = (curr.0 + 1, curr.1);
-            }
+            choice1 = (curr.0, curr.1 - 1);
+            choice2 = (curr.0 + 1, curr.1);
         }
         'J' => {
-            if pipes.contains(matrix[curr.0][curr.1 - 1]) {
-                next = (curr.0, curr.1 - 1);
-            } else {
-                next = (curr.0 - 1, curr.1);
-            }
+            choice1 = (curr.0, curr.1 - 1);
+            choice2 = (curr.0 - 1, curr.1);
         }
         '-' => {
-            if pipes.contains(matrix[curr.0][curr.1 - 1]) {
-                next = (curr.0, curr.1 - 1);
-            } else {
-                next = (curr.0, curr.1 + 1);
-            }
+            choice1 = (curr.0, curr.1 - 1);
+            choice2 = (curr.0, curr.1 + 1);
         }
         'F' => {
-            if pipes.contains(matrix[curr.0][curr.1 + 1]) {
-                next = (curr.0, curr.1 + 1);
-            } else {
-                next = (curr.0 + 1, curr.1);
-            }
+            choice1 = (curr.0, curr.1 + 1);
+            choice2 = (curr.0 + 1, curr.1);
         }
         _ => panic!("invalid current char"),
     }
+    if pipes.contains(matrix[choice1.0][choice1.1]) {
+        next = choice1;
+    } else {
+        next = choice2;
+    }
 
-    matrix[curr.0][curr.1] = '.';
+    matrix[curr.0][curr.1] = 'X';
+
     return next;
 }
 
--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,4 +1,4 @@
-use day10::day_10_1; //, day_10_2};
+use day10::{day_10_1, day_10_2};
 use std::fs;
 
 fn main() {
@@ -17,12 +17,43 @@ fn main() {
     println!("day 10_1 = {}", ans_10_1);
 
     // Part 2
-    //assert_eq!(day_10_2(short_text), );
+    assert_eq!(day_10_2(short_text), 1);
     //env::set_var("RUST_BACKTRACE", "1");
+    let area_example = "..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........";
+    assert_eq!(day_10_2(area_example), 4);
+    let example_2 = ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+";
+    assert_eq!(day_10_2(example_2), 8);
+    let example_3 = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
+    assert_eq!(day_10_2(example_3), 10);
 
-    /*
     let ans_10_2 = day_10_2(&text);
-    assert_eq!(ans_10_2, 1208);
+    assert_eq!(ans_10_2, 407);
     println!("day 10_2 = {}", ans_10_2);
-    */
 }
