4e600618f68a4f341eb8f6909e2b1ea1524a681d
j1642 <60148902+j1642@users.noreply.github.com>
2023-12-16 19:40:38 -0500

    ENH: Add 13.2

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -1,4 +1,6 @@
 pub fn day_13_1(text: &str) -> u32 {
+    // Return sum of vertical reflection indices plus sum of 100 * horizontal
+    // mirror indices.
     let mut sum = 0;
     let blocks = text.split("\n\n");
 
@@ -30,7 +32,6 @@ pub fn day_13_1(text: &str) -> u32 {
                 }
                 if is_shared {
                     shared_vert_mirror.push(mirror);
-                    //break;
                 }
             }
         }
@@ -38,11 +39,11 @@ pub fn day_13_1(text: &str) -> u32 {
             if shared_vert_mirror.len() > 1 {
                 panic!("more than one vertical mirror");
             }
-            // Average of palindrome start and end, plus one b/c mirror is between indices
             let add = shared_vert_mirror[0] as u32;
             sum += add;
             continue;
         }
+
         // If there is no vertical mirror, there is a horizontal mirror
         let mut horiz_mirrors: Vec<usize> = Vec::with_capacity(rows.len());
         for i in 0..rows.len() - 1 {
@@ -76,15 +77,143 @@ pub fn day_13_1(text: &str) -> u32 {
         if horiz_mirrors.len() > 1 {
             panic!("more than one horizontal mirror");
         }
-        if horiz_mirrors.len() > 0 {
-            sum += 100 * horiz_mirrors[0] as u32;
-        }
+        sum += 100 * horiz_mirrors[0] as u32;
     }
 
     return sum;
 }
 
+pub fn day_13_2(text: &str) -> u32 {
+    // Assuming there is one incorrect character in each input case, find the
+    // new lines of reflection and return their sum.
+    let mut sum = 0;
+    let blocks = text.split("\n\n");
+
+    for block in blocks {
+        let mut vertical_mirrors: Vec<Vec<usize>> = vec![];
+        let mut rows: Vec<Vec<char>> = vec![];
+
+        for line in block.lines() {
+            let line = line.chars().collect::<Vec<char>>();
+            let mirrors = find_vertical_mirrors(&line);
+            vertical_mirrors.push(mirrors);
+            rows.push(line);
+        }
+        let mut shared_vert_mirror: Vec<usize> = vec![];
+
+        // Centralize all vertical mirrors to avoid iterating over an incorrect
+        // row's mirrors
+        let mut all_mirrors: Vec<usize> = vec![];
+        for i in 0..vertical_mirrors.len() {
+            for j in 0..vertical_mirrors[i].len() {
+                // TODO: HashSet for O(1) lookup and insertion (from docs)
+                if !all_mirrors.contains(&vertical_mirrors[i][j]) {
+                    all_mirrors.push(vertical_mirrors[i][j]);
+                }
+            }
+        }
+        let mut count_shared_mirrors: Vec<usize> = vec![0; all_mirrors.len()];
+
+        // Make a histogram of mirrors
+        if all_mirrors.len() > 0 {
+            for i in 0..all_mirrors.len() {
+                let mirror = all_mirrors[i];
+                for j in 0..vertical_mirrors.len() {
+                    if vertical_mirrors[j].contains(&mirror) {
+                        count_shared_mirrors[i] += 1;
+                    }
+                }
+            }
+        }
+        // If a mirror is shared by all rows except one, that is the mirror we want
+        for i in 0..count_shared_mirrors.len() {
+            if count_shared_mirrors[i] == rows.len() - 1 {
+                shared_vert_mirror.push(all_mirrors[i]);
+            }
+        }
+        if shared_vert_mirror.len() > 0 {
+            let add = shared_vert_mirror[0] as u32;
+            sum += add;
+            continue;
+        }
+
+        // If there is no vertical mirror, there is a horizontal mirror
+        let mut horiz_mirrors: Vec<usize> = Vec::with_capacity(rows.len());
+        let mut is_diffs_used = false;
+
+        for i in 0..rows.len() - 1 {
+            let mut mirror_ind = 0;
+
+            let diffs = count_differences(&rows[i], &rows[i + 1]);
+            if diffs == 0 || (diffs == 1 && !is_diffs_used) {
+                if diffs == 1 {
+                    is_diffs_used = true;
+                }
+                mirror_ind = i + 1;
+                let mut hi = 0;
+                let mut lo = 0;
+                if i != 0 {
+                    lo = i - 1;
+                    hi = i + 2;
+                }
+                while hi < rows.len() {
+                    let diffs = count_differences(&rows[lo], &rows[hi]);
+                    if rows[lo] == rows[hi] || (diffs == 1 && !is_diffs_used) {
+                        if diffs == 1 {
+                            is_diffs_used = true;
+                        }
+                        if lo == 0 || hi == rows.len() - 1 {
+                            break;
+                        }
+                        lo -= 1;
+                        hi += 1;
+                    } else {
+                        mirror_ind = 0;
+                        // Missing the next line broke a few results
+                        is_diffs_used = false;
+                        break;
+                    }
+                }
+            }
+            if mirror_ind > 0 {
+                if is_diffs_used {
+                    horiz_mirrors.push(mirror_ind);
+                    break;
+                }
+            }
+        }
+        if horiz_mirrors.len() > 1 {
+            panic!("more than one horizontal mirror");
+        }
+        let prev_sum = sum;
+        sum += 100 * horiz_mirrors[0] as u32;
+        assert!(prev_sum != sum);
+    }
+
+    return sum;
+}
+
+fn count_differences(row1: &Vec<char>, row2: &Vec<char>) -> usize {
+    // Return number of different chars in two equal-length vectors
+    let mut count = 0;
+    if row1.len() != row2.len() {
+        panic!(
+            "inequal lengths: row1: {}, row2: {}",
+            row1.len(),
+            row2.len()
+        );
+    }
+    for i in 0..row1.len() {
+        if row1[i] != row2[i] {
+            count += 1;
+        }
+    }
+    return count;
+}
+
 fn find_vertical_mirrors(line: &Vec<char>) -> Vec<usize> {
+    // Find the set of mirrors within a single line of text. The returned values
+    // mark the index to the left of the mirror. The mirror is between indices.
     let mut palindromes = vec![];
 
     for left in 0..line.len() - 1 {
@@ -98,6 +227,7 @@ fn find_vertical_mirrors(line: &Vec<char>) -> Vec<usize> {
                 continue;
             }
             if is_palindrome(&line[left..=right]) {
+                // Center of the palindrome, plus one b/c mirror is between indices
                 palindromes.push((left + right) / 2 + 1);
             }
         }
