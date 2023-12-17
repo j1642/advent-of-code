8a3ce9edada5efe9a950d8dcf921c73255402d34
j1642 <60148902+j1642@users.noreply.github.com>
2023-12-17 14:12:47 -0500

    ENH: Add 13.2, use 1000 cycles to correctly approximate 1 billion

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -1,26 +1,86 @@
 pub fn day_14_1(text: &str) -> usize {
-    // Return total "load" after all round rocks roll north
+    // Return total structural load after all round rocks roll north
     let mut load = 0;
     let mut matrix = build_matrix(text);
 
+    roll_north(&mut matrix);
+
     for row in 0..matrix.len() {
         for col in 0..matrix[0].len() {
-            if matrix[row][col] == 'O' && row > 0 {
+            if matrix[row][col] == 'O' {
+                load += matrix.len() - row;
+            }
+        }
+    }
 
-                let mut prev_row = row - 1;
-                let mut curr_row = row;
-                while matrix[prev_row][col]  == '.' {
-                    matrix[prev_row][col] = 'O';
-                    matrix[curr_row][col] = '.';
-                    if prev_row == 0 {
+    return load;
+}
+
+pub fn day_14_2(text: &str) -> usize {
+    // Return total structural load after all round rocks roll N,W,S,E 1 billion times
+    let mut load = 0;
+    let mut matrix = build_matrix(text);
+
+    let mut empty_ind: usize;
+
+    // The final state is likely identical for 1000 cycles and 1 billion cycles
+    for _ in 0..1000 {
+        roll_north(&mut matrix);
+        // Roll west
+        for row in 0..matrix.len() {
+            empty_ind = 0;
+            for col in 0..matrix[0].len() {
+                if matrix[row][col] == '#' {
+                    if col == matrix[0].len() - 1 {
                         break;
                     }
-                    prev_row -= 1;
-                    curr_row -= 1;
+                    empty_ind = col + 1;
+                } else if matrix[row][col] == 'O' {
+                    matrix[row][col] = '.';
+                    matrix[row][empty_ind] = 'O';
+                    if empty_ind == matrix[0].len() - 1 {
+                        break;
+                    }
+                    empty_ind += 1;
                 }
-                // Not working yet
-                //println!("{} - {row}", matrix.len());
-                //load += matrix.len() - curr_row;
+            }
+        }
+        // Roll south, starting with the southern rows to avoid collisions
+        for col in 0..matrix[0].len() {
+            empty_ind = matrix.len() - 1;
+            for row in (0..matrix.len()).rev() {
+                if matrix[row][col] == '#' {
+                    if row == 0 {
+                        break;
+                    }
+                    empty_ind = row - 1;
+                } else if matrix[row][col] == 'O' {
+                    matrix[row][col] = '.';
+                    matrix[empty_ind][col] = 'O';
+                    if empty_ind == 0 {
+                        break;
+                    }
+                    empty_ind -= 1;
+                }
+            }
+        }
+        // Roll east, starting with the eastern columns to avoid collision
+        for row in 0..matrix.len() {
+            empty_ind = matrix[0].len() - 1;
+            for col in (0..matrix[0].len()).rev() {
+                if matrix[row][col] == '#' {
+                    if col == 0 {
+                        break;
+                    }
+                    empty_ind = col - 1;
+                } else if matrix[row][col] == 'O' {
+                    matrix[row][col] = '.';
+                    matrix[row][empty_ind] = 'O';
+                    if empty_ind == 0 {
+                        break;
+                    }
+                    empty_ind -= 1;
+                }
             }
         }
     }
@@ -31,10 +91,28 @@ pub fn day_14_1(text: &str) -> usize {
             }
         }
     }
-    for row in matrix {
-        println!("{:?}", row);
-    }
+
     return load;
+}
+
+fn roll_north(matrix: &mut Vec<Vec<char>>) {
+    // Roll north, starting with the northern rocks
+    let mut empty_ind;
+    for col in 0..matrix[0].len() {
+        empty_ind = 0;
+        for row in 0..matrix.len() {
+            if empty_ind > matrix.len() - 1 {
+                break;
+            }
+            if matrix[row][col] == '#' {
+                empty_ind = row + 1;
+            } else if matrix[row][col] == 'O' {
+                matrix[row][col] = '.';
+                matrix[empty_ind][col] = 'O';
+                empty_ind += 1;
+            }
+        }
+    }
 }
 
 fn build_matrix(text: &str) -> Vec<Vec<char>> {
--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,4 +1,4 @@
-use day14::day_14_1;//, day_14_2};
+use day14::{day_14_1, day_14_2};
 use std::fs;
 
 fn main() {
@@ -20,13 +20,12 @@ O.#..O.#.#
     assert_eq!(ans_14_1, 111339);
     println!("day 14_1 = {}", ans_14_1);
 
-    /*
     // Part 2
     //env::set_var("RUST_BACKTRACE", "1");
-    assert_eq!(day_14_2(example), 400);
+    let example_ans_2 = day_14_2(example);
+    assert_eq!(example_ans_2, 64);
 
     let ans_14_2 = day_14_2(&text);
     println!("day 14_2 = {}", ans_14_2);
-    assert_eq!(ans_14_2, 38063);
-    */
+    assert_eq!(ans_14_2, 93736);
 }
