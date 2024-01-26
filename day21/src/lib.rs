489bd6ebe00a7d205e2b40daef928d256e68738b
j1642 <60148902+j1642@users.noreply.github.com>
2024-01-25 20:49:33 -0500

    BUG: Finish 21.1 by adding another check to see if a coord was already processed

--- day21/src/lib.rs
+++ day21/src/lib.rs
@@ -1,26 +1,24 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::VecDeque;
+
 pub fn day21_1(text: &str, steps: u32) -> usize {
     // Return the amount of possible positions after taking X orthogonal
     // steps from the starting position
     let mut matrix: Vec<Vec<char>> = build_matrix(text);
     let start_coord = find_start_coord(&matrix).unwrap();
-    matrix[start_coord.row][start_coord.col] = 'X';
-
-    let mut unique_coords: HashSet<Coord> = HashSet::new();
-    if steps % 2 == 0 {
-        unique_coords.insert(Coord{row:start_coord.row, col: start_coord.col});
-    }
+    let mut count = 0;
 
     let mut q: VecDeque<(Coord, u32)> = VecDeque::new();
-    q.push_back((Coord{row: start_coord.row, col: start_coord.col}, steps));
+    q.push_back((start_coord, steps));
 
-    while q.len() > 0 {
-        let (coord, steps_remaining) = q.pop_front().unwrap();
+    while let Some((coord, steps_remaining)) = q.pop_front() {
         let row = coord.row;
         let col = coord.col;
+        if matrix[row][col] == 'X' {
+            continue;
+        }
         matrix[row][col] = 'X';
         if (steps - steps_remaining) % 2 == steps % 2 {
-            unique_coords.insert(Coord{row: row, col: col});
+            count += 1;
         }
         if steps_remaining < 1 {
             continue;
@@ -29,27 +27,39 @@ pub fn day21_1(text: &str, steps: u32) -> usize {
         let mut new_coord;
         // North
         if row > 0 && matrix[row - 1][col] == '.' {
-            new_coord = Coord{row: row - 1, col: col};
+            new_coord = Coord {
+                row: row - 1,
+                col: col,
+            };
             q.push_back((new_coord, steps_remaining - 1));
         }
         // South
         if row < matrix.len() - 1 && matrix[row + 1][col] == '.' {
-            new_coord = Coord{row: row + 1, col: col};
+            new_coord = Coord {
+                row: row + 1,
+                col: col,
+            };
             q.push_back((new_coord, steps_remaining - 1));
         }
         // East
         if col < matrix[0].len() - 1 && matrix[row][col + 1] == '.' {
-            new_coord = Coord{row: row, col: col + 1};
+            new_coord = Coord {
+                row: row,
+                col: col + 1,
+            };
             q.push_back((new_coord, steps_remaining - 1));
         }
         // West
         if col > 0 && matrix[row][col - 1] == '.' {
-            new_coord = Coord{row: row, col: col - 1};
+            new_coord = Coord {
+                row: row,
+                col: col - 1,
+            };
             q.push_back((new_coord, steps_remaining - 1));
         }
     }
 
-    return unique_coords.len();
+    return count;
 }
 
 fn build_matrix(text: &str) -> Vec<Vec<char>> {
@@ -71,7 +81,7 @@ fn find_start_coord(matrix: &Vec<Vec<char>>) -> Option<Coord> {
     for (i, row) in matrix.iter().enumerate() {
         for j in 0..row.len() {
             if matrix[i][j] == 'S' {
-                return Some(Coord{row: i, col: j});
+                return Some(Coord { row: i, col: j });
             }
         }
     }
--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,6 +1,6 @@
-use day21::day21_1;//, day21_2};
+use day21::day21_1; //, day21_2};
 use std::fs;
-use std::time;
+//use std::time;
 
 fn main() {
     let text = fs::read_to_string("day21.txt").expect("Should have read the file");
@@ -21,14 +21,14 @@ fn main() {
     assert_eq!(day21_1(example1, 3), 6);
     assert_eq!(day21_1(example1, 6), 16);
 
-    let start = time::Instant::now();
-    let ans_21_1 = day21_1(&text, 20);
-    let elapsed = start.elapsed();
-    println!("elapsed: {}ms", elapsed.as_millis());
+    //let start = time::Instant::now();
+    let ans_21_1 = day21_1(&text, 64);
+    //let elapsed = start.elapsed();
+    //println!("elapsed: {}ms", elapsed.as_millis());
     println!("day 21_1 = {}", ans_21_1);
-    /*
-    assert_eq!(ans_21_1, 812_609_846);
+    assert_eq!(ans_21_1, 3503);
 
+    /*
     // Part 2
     //env::set_var("RUST_BACKTRACE", "1");
 
