050f54027a0f55a742bde5ecd662c0dd56e016b6
j1642 <60148902+j1642@users.noreply.github.com>
2024-01-28 19:57:08 -0500

    MNT: Separate adding coordinates to the queue into a new function, add 21.2 func outline

--- day21/src/lib.rs
+++ day21/src/lib.rs
@@ -24,42 +24,84 @@ pub fn day21_1(text: &str, steps: u32) -> usize {
             continue;
         }
 
-        let mut new_coord;
-        // North
-        if row > 0 && matrix[row - 1][col] == '.' {
-            new_coord = Coord {
-                row: row - 1,
-                col: col,
-            };
-            q.push_back((new_coord, steps_remaining - 1));
-        }
-        // South
-        if row < matrix.len() - 1 && matrix[row + 1][col] == '.' {
-            new_coord = Coord {
-                row: row + 1,
-                col: col,
-            };
-            q.push_back((new_coord, steps_remaining - 1));
-        }
-        // East
-        if col < matrix[0].len() - 1 && matrix[row][col + 1] == '.' {
-            new_coord = Coord {
-                row: row,
-                col: col + 1,
-            };
-            q.push_back((new_coord, steps_remaining - 1));
-        }
-        // West
-        if col > 0 && matrix[row][col - 1] == '.' {
-            new_coord = Coord {
-                row: row,
-                col: col - 1,
-            };
-            q.push_back((new_coord, steps_remaining - 1));
-        }
+        add_nsew_to_queue(&matrix, &mut q, coord, steps_remaining);
     }
 
     return count;
+}
+
+pub fn day21_2(text: &str, steps: u32) -> usize {
+    // Return the amount of possible positions after taking X orthogonal
+    // steps from the starting position
+    let mut matrix: Vec<Vec<char>> = build_matrix(text);
+    let start_coord = find_start_coord(&matrix).unwrap();
+    let mut count = 0;
+    let width_height = 11;
+
+    let mut q: VecDeque<(Coord, u32)> = VecDeque::new();
+    q.push_back((start_coord, steps));
+
+    while let Some((coord, steps_remaining)) = q.pop_front() {
+        let row = coord.row;
+        let col = coord.col;
+        if matrix[row][col] == 'X' {
+            continue;
+        }
+        matrix[row][col] = 'X';
+        if (steps - steps_remaining) % 2 == steps % 2 {
+            count += 1;
+        }
+        if steps_remaining < 1 {
+            continue;
+        }
+
+        add_nsew_to_queue(&matrix, &mut q, coord, steps_remaining);
+    }
+
+    return count;
+}
+
+fn add_nsew_to_queue(
+    matrix: &Vec<Vec<char>>,
+    q: &mut VecDeque<(Coord, u32)>,
+    coord: Coord,
+    steps_remaining: u32,
+) {
+    let row = coord.row;
+    let col = coord.col;
+    let mut new_coord;
+    // North
+    if row > 0 && matrix[row - 1][col] == '.' {
+        new_coord = Coord {
+            row: row - 1,
+            col: col,
+        };
+        q.push_back((new_coord, steps_remaining - 1));
+    }
+    // South
+    if row < matrix.len() - 1 && matrix[row + 1][col] == '.' {
+        new_coord = Coord {
+            row: row + 1,
+            col: col,
+        };
+        q.push_back((new_coord, steps_remaining - 1));
+    }
+    // East
+    if col < matrix[0].len() - 1 && matrix[row][col + 1] == '.' {
+        new_coord = Coord {
+            row: row,
+            col: col + 1,
+        };
+        q.push_back((new_coord, steps_remaining - 1));
+    }
+    // West
+    if col > 0 && matrix[row][col - 1] == '.' {
+        new_coord = Coord {
+            row: row,
+            col: col - 1,
+        };
+        q.push_back((new_coord, steps_remaining - 1));
+    }
 }
 
 fn build_matrix(text: &str) -> Vec<Vec<char>> {
