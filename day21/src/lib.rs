234385e2e715cc048b27b0f0b7fdc7ebee300d4e
j1642 <60148902+j1642@users.noreply.github.com>
2024-01-28 21:00:20 -0500

    ENH: For 21.2, add a 5x5 matrix-building func, use a HashSet instead of mutable matrix

--- day21/src/lib.rs
+++ day21/src/lib.rs
@@ -1,10 +1,16 @@
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 
 pub fn day21_1(text: &str, steps: u32) -> usize {
     // Return the amount of possible positions after taking X orthogonal
     // steps from the starting position
     let mut matrix: Vec<Vec<char>> = build_matrix(text);
-    let start_coord = find_start_coord(&matrix).unwrap();
+    // Assume the input is a square with odd-length sides,
+    // and the start coord is the middle coordinate
+    assert_eq!(matrix.len(), matrix[0].len());
+    let start_coord = Coord {
+        row: matrix.len() / 2,
+        col: matrix[0].len() / 2,
+    };
     let mut count = 0;
 
     let mut q: VecDeque<(Coord, u32)> = VecDeque::new();
@@ -32,22 +38,30 @@ pub fn day21_1(text: &str, steps: u32) -> usize {
 
 pub fn day21_2(text: &str, steps: u32) -> usize {
     // Return the amount of possible positions after taking X orthogonal
-    // steps from the starting position
-    let mut matrix: Vec<Vec<char>> = build_matrix(text);
-    let start_coord = find_start_coord(&matrix).unwrap();
+    // steps from the starting position, assuming the input is
+    // infinitely repeating horizontally and vertically
+    //
+    // TODO: use u8 in matrix instead of 4-byte chars
+    let matrix: Vec<Vec<char>> = build_5x5_matrix(text);
+    let start_coord = Coord {
+        row: matrix.len() / 2,
+        col: matrix[0].len() / 2,
+    };
     let mut count = 0;
-    let width_height = 11;
+    let width_height = matrix.len();
 
+    let mut checked: HashSet<Coord> = HashSet::new();
     let mut q: VecDeque<(Coord, u32)> = VecDeque::new();
     q.push_back((start_coord, steps));
 
     while let Some((coord, steps_remaining)) = q.pop_front() {
+        // TODO: use modulo to wrap-around instead of hitting the matrix edge
         let row = coord.row;
         let col = coord.col;
-        if matrix[row][col] == 'X' {
+        if checked.contains(&coord) {
             continue;
         }
-        matrix[row][col] = 'X';
+        checked.insert(coord);
         if (steps - steps_remaining) % 2 == steps % 2 {
             count += 1;
         }
@@ -55,7 +69,39 @@ pub fn day21_2(text: &str, steps: u32) -> usize {
             continue;
         }
 
-        add_nsew_to_queue(&matrix, &mut q, coord, steps_remaining);
+        let mut new_coord;
+        // North
+        if row > 0 && matrix[row - 1][col] == '.' {
+            new_coord = Coord {
+                row: row - 1,
+                col: col,
+            };
+            q.push_back((new_coord, steps_remaining - 1));
+        }
+        // South
+        if row < matrix.len() - 1 && matrix[row + 1][col] == '.' {
+            new_coord = Coord {
+                row: row + 1,
+                col: col,
+            };
+            q.push_back((new_coord, steps_remaining - 1));
+        }
+        // East
+        if col < matrix[0].len() - 1 && matrix[row][col + 1] == '.' {
+            new_coord = Coord {
+                row: row,
+                col: col + 1,
+            };
+            q.push_back((new_coord, steps_remaining - 1));
+        }
+        // West
+        if col > 0 && matrix[row][col - 1] == '.' {
+            new_coord = Coord {
+                row: row,
+                col: col - 1,
+            };
+            q.push_back((new_coord, steps_remaining - 1));
+        }
     }
 
     return count;
@@ -113,19 +159,32 @@ fn build_matrix(text: &str) -> Vec<Vec<char>> {
     return matrix;
 }
 
+fn build_5x5_matrix(text: &str) -> Vec<Vec<char>> {
+    let mut matrix = build_matrix(text);
+    // Change start coord from 'S' to '.'
+    let orig_width_height = matrix.len();
+    let middle_row_idx = matrix.len() / 2;
+    let middle_col_idx = matrix[0].len() / 2;
+    matrix[middle_row_idx][middle_col_idx] = '.';
+
+    // Change width to 5x original width
+    for i in 0..matrix.len() {
+        matrix[i].extend_from_within(0..orig_width_height);
+        matrix[i].extend_from_within(0..orig_width_height);
+        matrix[i].extend_from_within(0..orig_width_height);
+        matrix[i].extend_from_within(0..orig_width_height);
+    }
+    // Change height to 5x original height
+    matrix.extend_from_within(0..orig_width_height);
+    matrix.extend_from_within(0..orig_width_height);
+    matrix.extend_from_within(0..orig_width_height);
+    matrix.extend_from_within(0..orig_width_height);
+
+    return matrix;
+}
+
 #[derive(Debug, Hash, Eq, PartialEq)]
 struct Coord {
     row: usize,
     col: usize,
-}
-
-fn find_start_coord(matrix: &Vec<Vec<char>>) -> Option<Coord> {
-    for (i, row) in matrix.iter().enumerate() {
-        for j in 0..row.len() {
-            if matrix[i][j] == 'S' {
-                return Some(Coord { row: i, col: j });
-            }
-        }
-    }
-    return None;
 }
