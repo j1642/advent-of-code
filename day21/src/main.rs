323ed79310584dbfa1497bcdb2f8bd8134771899
j1642 <60148902+j1642@users.noreply.github.com>
2024-01-30 22:42:25 -0500

    MNT: Use i64 to prevent dropping negative numbers, add TODO for continuing investigation

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -32,7 +32,10 @@ fn main() {
     assert_eq!(day21_2(example1, 50), 1594);
     assert_eq!(day21_2(example1, 100), 6536);
 
-    lagrange_interpolation(example1);
+    lagrange_interpolation(example1, 500);
+    lagrange_interpolation(example1, 1000);
+    lagrange_interpolation(example1, 5000);
+    println!("expect: 167_004, 668_697, 16_733_044");
     /*
     assert_eq!(day21_2(example1, 500), 167004);
     assert_eq!(day21_2(example1, 1000), 668697);
@@ -44,23 +47,36 @@ fn main() {
     */
 }
 
-fn lagrange_interpolation(text: &str) {
+fn lagrange_interpolation(text: &str, input: u64) {
     let (first_line, _) = text.split_once('\n').unwrap();
     let width_height = first_line.chars().count() as u64;
 
+    // TODO: make func to get custom matrix dimensions, 1x1, 2x2, 3x3
+    let x = input;
+    let steps = input;
+    //let n = (steps - width_height / 2) / width_height;
+    for n in [2, 4, 6] {
+        //let d = n * width_height + width_height / 2;
+        //println!("n={n}, d={d}");
+    }
+
     let mut x_vals: Vec<u64> = vec![];
-    for i in 0..6 {
+    for i in [2, 4, 6] {
         x_vals.push(width_height / 2 + width_height * i);
     }
+    println!("x_vals: {:?}", x_vals);
+
     let mut y_vals: Vec<u64> = vec![];
     for x in &x_vals {
         y_vals.push(day21_2(text, *x as u32) as u64);
+        // Below produced expected result, so Lagrange interp.
+        // seems to be implemented well
+        //y_vals.push((*x * *x) as u64);
     }
     println!("y_vals: {:?}", y_vals);
     assert_eq!(x_vals.len(), y_vals.len());
 
     // solve for lagrange interp. with input 500
-    let x = 500;
     let mut pdt_input_minus_x_vals = 1;
     for i in 0.. x_vals.len() {
         pdt_input_minus_x_vals *= x - x_vals[i]
@@ -68,7 +84,8 @@ fn lagrange_interpolation(text: &str) {
     let mut result: i64 = 0;
 
     for i in 0..x_vals.len() {
-        let numerator = pdt_input_minus_x_vals / (x - x_vals[i]) * y_vals[i];
+        let mut numerator: i64 = pdt_input_minus_x_vals as i64 / (x as i64 - x_vals[i] as i64);
+        numerator *= y_vals[i] as i64;
 
         let mut denom: i64 = 1;
         for j in 0..x_vals.len() {
@@ -86,5 +103,5 @@ fn lagrange_interpolation(text: &str) {
         result += numerator as i64 / denom;
     }
 
-    println!("{}", result);
+    println!("result: {}", result);
 }
