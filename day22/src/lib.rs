09c67a73a864660e3d173b17b4395c843163e793
j1642 <60148902+j1642@users.noreply.github.com>
2024-02-12 22:14:08 -0500

    BUG: Fix typo so the upper-most block detects collisions

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -1,6 +1,6 @@
 use std::cmp::min;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct Point {
     x: usize,
     y: usize,
@@ -16,13 +16,14 @@ struct Block {
 // Assumptions:
 // - blocks have dimensions 1x1xN
 
-// TODO: blocks fall into their final positions
-// TODO: determine points contained in a block (ish, not a literal vec of points)
-// TODO: determine which bricks touch each other (naive n^2)
+// TODO: determine which bricks touch each other (naive n^2) - sort of implemented
 // TODO: determine which bricks can be destroyed without other
 //    bricks falling down
 pub fn day22_1(text: &str) -> u32 {
     let mut blocks = find_blocks(text);
+    for (i, block) in blocks.iter().enumerate() {
+        println!("i: {i},  {:?}", block);
+    }
     drop_blocks(&mut blocks);
     for (i, block) in blocks.iter().enumerate() {
         println!("i: {i},  {:?}", block);
@@ -31,45 +32,66 @@ pub fn day22_1(text: &str) -> u32 {
 }
 
 fn drop_blocks(blocks: &mut Vec<Block>) {
-    // blocks fall into their final positions
+    // Decrease each block's elavation until it rests on another block or z=1
 
-    // sort by z, deal with lowest blocks first
+    // Adjust the lowest elavation blocks first after sorting
     // What about blocks aligned on z-axis? - use lower z
-    blocks.sort_by(|a, b| 
-        min(a.start.z, a.end.z).partial_cmp(min(&b.start.z, &b.end.z)).unwrap());
+    blocks.sort_by(|a, b| {
+        min(a.start.z, a.end.z)
+            .partial_cmp(min(&b.start.z, &b.end.z))
+            .unwrap()
+    });
 
     for i in 0..blocks.len() {
         let mut block = blocks[i].clone();
-        // lowest elavation block starts at z=0
+        if block.start == block.end {
+            panic!("block start == block end");
+        }
+        // lowest elavation block starts at z=1 because of the examples
         if i == 0 {
-            if blocks[0].start.z > 0 {
-                let start_end_diff = blocks[0].end.z - blocks[0].start.z;
-                blocks[0].start.z = 0;
-                blocks[0].end.z = start_end_diff;
+            if block.start.z > 1 {
+                let start_end_diff = block.end.z - block.start.z;
+                block.start.z = 1;
+                block.end.z = start_end_diff;
             }
-            println!("first: {:?}", block);
             blocks[i] = block;
             continue;
         }
 
         // Block falls to just above its lower neighbor
         if block.start.z > (1 + blocks[i - 1].end.z) {
-            let start_end_diff = blocks[0].end.z - blocks[0].start.z;
+            let start_end_diff = block.end.z - block.start.z;
             block.start.z = 1 + blocks[i - 1].end.z;
             block.end.z = block.start.z + start_end_diff;
+        }
+        // TODO: remove at the end
+        if block.start == block.end {
+            panic!("block start == block end: {:?}", block.start);
         }
 
         let mut top_cubes: Vec<Point> = vec![];
         if block.start.x != block.end.x {
             for x in block.start.x..=block.end.x {
-                top_cubes.push(Point{x: x, y: block.start.y, z: block.start.z});
+                top_cubes.push(Point {
+                    x: x,
+                    y: block.start.y,
+                    z: block.start.z,
+                });
             }
         } else if block.start.y != block.end.y {
             for y in block.start.y..=block.end.y {
-                top_cubes.push(Point{x: block.start.x, y: y, z: block.start.z});
+                top_cubes.push(Point {
+                    x: block.start.x,
+                    y: y,
+                    z: block.start.z,
+                });
             }
         } else if block.start.z != block.end.z {
-            top_cubes.push(Point{x: block.start.x, y: block.start.y, z: block.start.z});
+            top_cubes.push(Point {
+                x: block.start.x,
+                y: block.start.y,
+                z: block.start.z,
+            });
         }
 
         // while there is no touched block below, decrement start and end z
@@ -77,17 +99,28 @@ fn drop_blocks(blocks: &mut Vec<Block>) {
             let mut bottom_cubes: Vec<Point> = vec![];
             if blocks[j].start.x != blocks[j].end.x {
                 for x in blocks[j].start.x..=blocks[j].end.x {
-                    bottom_cubes.push(Point{x: x, y: blocks[j].start.y, z: blocks[j].start.z});
+                    bottom_cubes.push(Point {
+                        x: x,
+                        y: blocks[j].start.y,
+                        z: blocks[j].start.z,
+                    });
                 }
             } else if blocks[j].start.y != blocks[j].end.y {
                 for y in blocks[j].start.y..=blocks[j].end.y {
-                    bottom_cubes.push(Point{x: blocks[j].start.x, y: y, z: blocks[j].start.z});
+                    bottom_cubes.push(Point {
+                        x: blocks[j].start.x,
+                        y: y,
+                        z: blocks[j].start.z,
+                    });
                 }
             } else if blocks[j].start.z != blocks[j].end.z {
-                bottom_cubes.push(Point{x: blocks[j].start.x, y: blocks[j].start.y, z: blocks[j].start.z});
+                bottom_cubes.push(Point {
+                    x: blocks[j].start.x,
+                    y: blocks[j].start.y,
+                    z: blocks[j].start.z,
+                });
             }
 
-            // TODO: fix collision detection bug
             // Consider intersection of sets instead of n^2 search
             let mut can_fall_more = true;
             for top_cube in &top_cubes {
@@ -98,6 +131,7 @@ fn drop_blocks(blocks: &mut Vec<Block>) {
                         println!("top: {:?}, bott: {:?}", top_cube, bottom_cube);
                         can_fall_more = false;
                         break;
+                        // TODO: instead of breaking, continue to find all neighbors
                     }
                 }
                 if !can_fall_more {
@@ -111,11 +145,14 @@ fn drop_blocks(blocks: &mut Vec<Block>) {
             if can_fall_more {
                 block.start.z -= 1;
                 block.end.z -= 1;
+                println!(
+                    "decrementing block {i}, start.z={}, end.z={}",
+                    block.start.z, block.end.z
+                );
             }
-            if block.start.z == 0 {
+            if block.start.z == 1 {
                 break;
             }
-
         }
 
         // Replace old block with the changed clone. Avoids mixing (im)mutable borrowing
