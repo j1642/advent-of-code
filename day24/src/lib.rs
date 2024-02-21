4e4551f383b90eeb4a3c3945f4b8f5fb1366467a
j1642 <60148902+j1642@users.noreply.github.com>
2024-02-21 11:27:44 -0500

    ENH: Add 24.1

--- day24/src/lib.rs
+++ day24/src/lib.rs
@@ -8,31 +8,70 @@ struct Hailstone {
     vel_z: f32,
 }
 
-pub fn day24_1(text: &str) -> u32 {
+pub fn day24_1(text: &str, zone_min: f32, zone_max: f32) -> u32 {
     // How many hailstone paths cross within the zone? Ignore z for part one
-    // example test zone: 7 <= x, y <= 27
     let hail = build_hailstones(text);
+    let mut count_intersections = 0;
 
-    // TODO: solve system of equations to find intersection, if slopes not identical
-    // TODO: determine if intersection will happen in the future (include) or the past (exclude)
+    // Solve system of equations to find intersection, if slopes not parallel
     for i in 0..hail.len() - 1 {
         let slope1 = hail[i].vel_y / hail[i].vel_x;
         let intercept1 = hail[i].y - slope1 * hail[i].x;
-        println!("y1 = {slope1}x + {intercept1}");
         for j in (i + 1)..hail.len() {
             let slope2 = hail[j].vel_y / hail[j].vel_x;
             let intercept2 = hail[j].y - slope2 * hail[j].x;
-            //println!("y2 = {slope2}x + {intercept2}");
+            if slope1 == slope2 {
+                continue;
+            }
+            let x_meet = (intercept1 - intercept2) / (slope2 - slope1);
+            let y_meet = slope1 * x_meet + intercept1;
+            // Determine if the intersection is in the path of both rays
+            let mut will_intersect = true;
+            for (slope, stone) in [(slope1, &hail[i]), (slope2, &hail[j])] {
+                if slope >= 0.0 {
+                    if stone.vel_x < 0.0 && stone.vel_y < 0.0 {
+                        if x_meet > stone.x || y_meet > stone.y {
+                            will_intersect = false;
+                            break;
+                        }
+                    } else if x_meet < stone.x || y_meet < stone.y {
+                        will_intersect = false;
+                        break;
+                    }
+                } else if slope < 0.0 {
+                    if stone.vel_x < 0.0 {
+                        if x_meet > stone.x {
+                            will_intersect = false;
+                            break;
+                        }
+                    } else if stone.vel_y < 0.0 {
+                        if y_meet > stone.y {
+                            will_intersect = false;
+                            break;
+                        }
+                    } else {
+                        panic!();
+                    }
+                }
+            }
+            if !will_intersect {
+                continue;
+            }
+
+            if zone_min <= x_meet && x_meet <= zone_max && zone_min <= y_meet && y_meet <= zone_max
+            {
+                count_intersections += 1;
+            }
         }
     }
 
-    return 0;
+    return count_intersections;
 }
 
 fn build_hailstones(text: &str) -> Vec<Hailstone> {
     let mut hail = vec![];
     for line in text.lines() {
-        let (mut locations, mut velocities) = line.split_once(" @ ").unwrap();
+        let (locations, velocities) = line.split_once(" @ ").unwrap();
         let locations = locations
             .split(", ")
             .map(|s| s.trim())
--- day24/src/main.rs
+++ day24/src/main.rs
@@ -2,18 +2,19 @@ use day24::day24_1; //, day24_2};
 use std::fs;
 
 fn main() {
-    //let text = fs::read_to_string("day24.txt").expect("Should have read the file");
+    let text = fs::read_to_string("day24.txt").expect("Should have read the file");
     let example = "19, 13, 30 @ -2,  1, -2
 18, 19, 22 @ -1, -1, -2
 20, 25, 34 @ -2, -2, -4
 12, 31, 28 @ -1, -2, -1
 20, 19, 15 @  1, -5, -3";
 
-    assert_eq!(day24_1(example), 2);
+    // check for intersections within 7 <= (x, y) <= 27
+    assert_eq!(day24_1(example, 7.0, 27.0), 2);
 
-    //let ans_24_1 = day24_1(&text);
-    //println!("day 24_1 = {}", ans_24_1);
-    //assert_eq!(ans_24_1, 2470);
+    let ans_24_1 = day24_1(&text, 200000000000000.0, 400000000000000.0);
+    println!("day 24_1 = {}", ans_24_1);
+    assert_eq!(ans_24_1, 21679);
 
     /*
     // Part 2
