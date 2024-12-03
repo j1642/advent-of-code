d3658d3c909089b454a61e5f515a8c0415160598
j1642 <60148902+j1642@users.noreply.github.com>
2024-12-03 17:14:36 -0500

    ENH: Add partial 3.2

--- aoc2024/day03/src/lib.rs
+++ aoc2024/day03/src/lib.rs
@@ -1,3 +1,5 @@
+use std::str::Chars;
+
 use regex::Regex;
 
 pub fn day03_1(text: &str) -> i32 {
@@ -17,4 +19,83 @@ pub fn day03_1(text: &str) -> i32 {
     }
 
     return sum_of_pdts;
+}
+
+pub fn day03_2(text: &str) -> u32 {
+    let mut all_chars = text.chars();
+    return recurse(&mut all_chars);
+}
+
+fn recurse(all_chars: &mut Chars) -> u32 {
+    let mut sum_of_pdts = 0;
+    let mut is_enabled = true;
+
+    while let Some(c) = all_chars.next() {
+    //for c in all_chars {
+        if c == 'm' {
+            match found_m(all_chars) {
+                None=>{continue},
+                Some(num) => {
+                    sum_of_pdts += num;
+                },
+            }
+        } else if c == 'd' {
+            //look for o
+            // is_enabled = found_d()
+        }
+    }
+
+    return sum_of_pdts;
+}
+
+fn found_m(all_chars: &mut Chars) -> Option<u32> {
+    let mut is_valid = true;
+    let keys = ['u', 'l', '('];
+    for i in 0..keys.len() {
+        let next = all_chars.next().unwrap();
+        if next != keys[i] {
+            is_valid = false;
+            break;
+        }
+    }
+
+    println!("found mul(");
+    if is_valid {
+        return get_product(all_chars);
+    }
+    println!("returning None from found_m()");
+    return None
+}
+
+fn get_product(all_chars: &mut Chars) -> Option<u32> {
+    let mut next: char;
+    let mut is_valid = true;
+    let mut num: u32 = 0;
+    let mut first = 0;
+    let mut second = 0;
+
+    while is_valid {
+        next = all_chars.next().unwrap();
+        if next.is_digit(10) {
+            num *= 10;
+            num += next.to_digit(10).unwrap();
+        } else {
+            if next == ',' && first == 0 && second == 0 {
+                first = num;
+                println!("first num: {}", first);
+                num = 0;
+            } else if next == ')' {//&& first != 0 && second == 0 {
+                //println!("{}, {}", first, second);
+                second = num;
+                println!("2nd num: {}", second);
+                // is_valid still true
+                break;
+            } else {
+                is_valid = false;
+                println!("invalidated at '{}'", next);
+            }
+        }
+    }
+
+    return Some(first * second);
 }
--- aoc2024/day03/src/main.rs
+++ aoc2024/day03/src/main.rs
@@ -1,4 +1,4 @@
-use day03::day03_1;
+use day03::{day03_1, day03_2};
 use std::fs;
 
 fn main() {
@@ -8,8 +8,9 @@ fn main() {
     println!("day 3.1 = {day3_1_ans}");
     assert_eq!(day3_1_ans, 173731097);
 
+    let test = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    assert_eq!(day03_2(test), 48);
     /*
-    assert_eq!(day03_3(test), 4);
     let day03_3_ans = day03_3(&text);
     println!("day 3.3 = {day03_3_ans}");
     assert_eq!(day03_3_ans, 381);
